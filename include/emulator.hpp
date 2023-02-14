0d18775bbf7bccb03ca09453c2bd65c869f7885d
Eduardo Antunes dos Santos Vieira <eduardoantunes986@gmail.com>
2023-02-14 00:51:08 -0300

    Refactor and general bug fixes

--- include/emulator.hpp
+++ include/emulator.hpp
@@ -14,27 +14,51 @@
 #include "screen.hpp"
 #include "keypad.hpp"
 
-// The real meat of the emulator
+// This class represents the emulator itself; the CPU, its state
+// and its peripherals. It also includes a variety of utility functions.
 
 namespace chip8 {
     class Emulator {
         public:
             Emulator();
-            Emulator(const std::vector<uint8_t> &code) : Emulator() { load_code(code); }
-            void load_code(const std::vector<uint8_t> &code);
+            Emulator(const std::vector<uint8_t> &prog) 
+                : Emulator() { load_prog(prog); }
+            // Load program into RAM
+            void load_prog(const std::vector<uint8_t> &prog);
+            // Run currently loaded program
             int run();
+
         private:
-            // font is in the address range 0x50 - 0x9F
+            // Address of the built-in fontset
             static const uint16_t font_addr = 0x50;
+            // Starting address of the program
+            static const uint16_t prog_addr = 0x200;
 
-            // CPU state:
-            uint8_t memory[4096] = {0}, v[16] = {0};
-            uint8_t delay_timer = 0, sound_timer = 0;
-            uint16_t pc, index_reg, sp = 0;
+            // 4KiB of RAM for program data
+            uint8_t ram[4096] = {0};
+            // Program counter, which contains the address of the next 
+            // instruction to be executed and thus is used for flow control
+            uint16_t pc = prog_addr;
+            // Index register, used to point at stuff in memory
+            uint16_t index = 0;
+            // Stack pointer and stack, for subroutines
             uint16_t stack[16] = {0};
+            uint16_t sp = 0;
+            // Delay timer, a counter that is simply decremented at a constant
+            // speed; useful for timing stuff
+            uint8_t delay = 0;
+            // Sound timer, another counter that is decremented at a constant 
+            // speed, except it also dictates whether sound is played
+            uint8_t sound = 0;
+            // Variable registers, 16 general purpose registers, except for the
+            // last one, which doubles as a flag register
+            uint8_t v[16] = {0};
 
-            void set_flag(int f) { v[15] = f ? 1 : 0; }
+            // Set or clear the flag register, VF
+            void set_flag(bool f) { v[0xF] = f ? 1 : 0; }
+            // Update both timers
             void update_timers();
+            // Single step through the program
             int single_step();
 
             // Input and output:
--- src/emulator.cpp
+++ src/emulator.cpp
@@ -16,7 +16,7 @@
 using namespace chip8;
 
 Emulator::Emulator() {
-    pc = 0x200; // user code starts here
+    // Built-in fontset; it only includes the 16 hexadecimal characters
     uint8_t font[80] = {
         0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
         0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -35,25 +35,23 @@ Emulator::Emulator() {
         0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
         0xF0, 0x80, 0xF0, 0x80, 0x80  // F
     };
-    // load font into the correct addres range
+    // Load built-in font into RAM, starting from font_addr
     for(uint16_t i = 0; i < 80; ++i)
-        memory[i + font_addr] = font[i];
+        ram[i + font_addr] = font[i];
 }
 
-void Emulator::load_code(const std::vector<uint8_t> &code) {
-    // load code into memory, starting from the address 0x200
-    uint16_t addr = 0x200;
-    for(auto byte : code) {
-        memory[addr] = byte;
-        ++addr;
-    }
+void Emulator::load_prog(const std::vector<uint8_t> &prog) {
+    // Load code into RAM, starting from prog_addr
+    uint16_t addr = prog_addr;
+    for(auto byte : prog)
+        ram[addr++] = byte;
 }
 
 #define INSTS_PER_SECOND 700
 
 #define GET_TICKS() SDL_GetTicks()
 
-#define SLEEP(ms) SDL_Delay(ms)
+#define SLEEP(t) SDL_Delay(t)
 
 int Emulator::run() {
     int status = 0;
@@ -87,13 +85,18 @@ int Emulator::run() {
 }
 
 #undef INSTS_PER_SECOND
+
 #undef GET_TICKS
+
 #undef SLEEP
 
 void Emulator::update_timers() {
-    if(delay_timer > 0) --delay_timer;
-    if(sound_timer > 0) {
-        --sound_timer;
+    // The delay timer is constantly decremented while it's above zero
+    if(delay > 0) --delay;
+    // That's also the case for the sound timer, but, as long as it is above 
+    // zero, a beeping sound must be played
+    if(sound > 0) {
+        --sound;
         audio.play();
     } else {
         audio.pause();
@@ -110,134 +113,144 @@ void Emulator::update_timers() {
 #define GET_NIB(byte, n) ((n) ? ((byte) & 0x0F) : (((byte) & 0xF0) >> 4))
 
 int Emulator::single_step() {
-    // Fetch an instruction, break it and advance the PC
-    uint16_t inst = JOIN_BYTES(memory[pc], memory[pc + 1]);
-    uint8_t op    = GET_NIB(memory[pc], 0);
-    uint8_t x     = GET_NIB(memory[pc], 1);
-    uint8_t y     = GET_NIB(memory[pc + 1], 0);
-    uint8_t n     = GET_NIB(memory[pc + 1], 1);
-    uint8_t nn    = memory[pc + 1];
-    uint16_t nnn  = inst & 0x0FFF;
+    // Fetch the 16 bits of an instruction and advance the PC
+    uint8_t first = ram[pc], second = ram[pc + 1];
+    uint16_t inst = JOIN_BYTES(first, second);
     pc += 2;
-    // Decode and execute instruction
+    // Break the instruction into its meaningful parts
+    uint8_t op   = GET_NIB(first, 0);
+    uint8_t x    = GET_NIB(first, 1);
+    uint8_t y    = GET_NIB(second, 0);
+    uint8_t n    = GET_NIB(second, 1);
+    uint8_t nn   = second;
+    uint16_t nnn = inst & 0x0FFF;
+    // Decode and execute
     switch(op) {
-        case 0:
-            if(inst == 0x00E0) {
-                // 0x00E0: clear the display
-                screen.clear();
-            } else if(inst == 0x00EE) {
-                // 0x00EE: return from subroutine
-                pc = stack[--sp];
+        case 0x0:
+            // Multiple instructions possible
+            switch(inst) {
+                case 0x00E0:
+                    // 0x00E0: clear the display
+                    screen.clear();
+                    break;
+                case 0x00EE:
+                    // 0x00EE: return from subroutine
+                    pc = stack[--sp];
+                    break;
+                default:
+                    fprintf(stderr, "Unrecognized instruction: %4X\n", inst);
             }
             break;
-        case 1:
+        case 0x1:
             // 0x1NNN: unconditional jump to NNN
             pc = nnn;
             break;
-        case 2:
+        case 0x2:
             // 0x2NNN: jump to subroutine
             stack[sp++] = pc;
             pc = nnn;
             break;
-        case 3:
-            // 0x3XNN: skip next instruction if V[X] == NN
+        case 0x3:
+            // 0x3XNN: skip next instruction if VX == NN
             if(v[x] == nn) pc += 2;
             break;
-        case 4:
-            // 0x4NNN: skip next instruction if V[X] != NN
+        case 0x4:
+            // 0x4NNN: skip next instruction if VX != NN
             if(v[x] != nn) pc += 2;
             break;
-        case 5:
-            // 0x5XY0: skip next instruction if V[X] == V[Y]
+        case 0x5:
+            // 0x5XY0: skip next instruction if VX == VY
             if(n == 0 && v[x] == v[y]) pc += 2;
             break;
-        case 6:
-            // 0x6XNN: set register V[X] to NN
+        case 0x6:
+            // 0x6XNN: set register VX to NN
             v[x] = nn;
             break;
-        case 7:
-            // 0x7XNN: add NN to register V[X], don't change flag
+        case 0x7:
+            // 0x7XNN: add NN to register VX, never set flag
             v[x] += nn;
             break;
-        case 8:
-            // Arithmetic and logical instructions
+        case 0x8:
+            // Multiple instructions possible
             switch(n) {
-                case 0:
-                    // 0x8XY0: set V[X] to V[Y]
+                case 0x0:
+                    // 0x8XY0: set VX to VY
                     v[x] = v[y];
                     break;
-                case 1:
-                    // 0x8XY1: set V[X] to V[X] | V[Y]
+                case 0x1:
+                    // 0x8XY1: set VX to VX | VY
                     v[x] |= v[y];
                     break;
-                case 2:
-                    // 0x8XY2: set V[X] to V[X] & V[Y]
+                case 0x2:
+                    // 0x8XY2: set VX to VX & VY
                     v[x] &= v[y];
                     break;
-                case 3:
-                    // 0x8XY3: set V[x] to V[X] ^ V[Y]
+                case 0x3:
+                    // 0x8XY3: set Vx to VX ^ VY
                     v[x] ^= v[y];
                     break;
-                case 4:
-                    // 0x8XY4: add V[Y] to V[X], set flag on overflow
-                    set_flag(v[x] == 255);
+                case 0x4:
+                    // 0x8XY4: add VY to VX, set flag on overflow
+                    set_flag(v[x] + v[y] > 255);
                     v[x] += v[y];
                     break;
-                case 5:
-                    // 0x8XY5: subtract V[Y] from V[X], clear flag on borrow
+                case 0x5:
+                    // 0x8XY5: subtract VY from VX, clear flag on borrow
                     set_flag(v[x] >= v[y]);
                     v[x] -= v[y];
                     break;
-                case 6:
-                    // 0x8XY6: bitwise shift V[X] to the right, set flag to bit
+                case 0x6:
+                    // 0x8XY6: bitwise shift VX to the right, set flag to bit
                     // that was shifted out. NOTE: this instruction behaves
                     // differently in different variants of chip-8; here we
                     // adopt the chip-48 behavior instead of the original
-                    set_flag(v[x] & 1);
+                    set_flag(v[x] & 0x01);
                     v[x] >>= 1;
                     break;
-                case 7:
-                    // 0x8XY7: subtract V[X] from V[Y], clear flag on borrow
+                case 0x7:
+                    // 0x8XY7: subtract VX from VY, clear flag on borrow
                     set_flag(v[y] >= v[x]);
                     v[y] -= v[x];
                     break;
-                case 14:
-                    // 0x8XYE: bitwise shift V[X] to the left, set flag to bit
+                case 0xE:
+                    // 0x8XYE: bitwise shift VX to the left, set flag to bit
                     // that was shifted out. NOTE: this instruction behaves
                     // differently in different variants of chip-8; here we
                     // adopt the chip-48 behavior instead of the original
-                    set_flag(v[x] & 128);
+                    set_flag(v[x] & 0x80);
                     v[x] <<= 1;
                     break;
                 default:
-                    std::cerr << "Unrecognized instruction" << std::endl;
+                    fprintf(stderr, "Unrecognized instruction: %4X\n", inst);
             }
-        case 9:
-            // 0x9XY0: skip next instruction if V[X] != V[Y]
+            break;
+        case 0x9:
+            // 0x9XY0: skip next instruction if VX != VY
             if(n == 0 && v[x] != v[y]) pc += 2;
             break;
-        case 10:
+        case 0xA:
             // 0xANNN: set index register to NNN
-            index_reg = nnn;
+            index = nnn;
             break;
-        case 11:
+        case 0xB:
             // 0xBNNN: jump with offset. NOTE: this instruction is different in
             // different versions of chip-8; here, we adopt the original chip-8
-            // instruction, which jumps to NNN + V[0]
+            // behavior, which jumps to NNN + V0
             pc = nnn + v[0];
             break;
-        case 12:
+        case 0xC:
             // 0xCXNN: generate random number, bitwise and it with NN and store
-            // the result in V[X]
+            // the result in VX
             v[x] = rand() & nn;
             break;
-        case 13: {
+        case 0xD: {
             // 0xDXYN: draw N pixels tall sprite, found at the memory address
-            // specified by the index register, to the position (V[X], V[Y]) of
+            // specified by the index register, to the position (VX, VY) of
             // the display, setting a flag if any pixel is erased during this
-            set_flag(0);
+            set_flag(false);
             uint8_t x_pos = v[x] & (screen.width - 1);
             uint8_t y_pos = v[y] & (screen.height - 1);
+            const uint8_t original_x = x_pos;
 
             // Sprites are stored in memory as a series of bytes, where each
             // byte represents the operations that must be done to draw a line
@@ -247,13 +260,13 @@ int Emulator::single_step() {
             // (i.e. if it's on it must be erased, if it is off it must be
             // filled). Otherwise, the pixel is kept as is.
             for(int i = 0; i < n; ++i) {
-                uint8_t line = memory[index_reg + i];
+                uint8_t line = ram[index + i];
                 for(int j = 0; j < 8; ++j) {
                     int pixel = GET_BIT(line, j);
                     if(pixel) {
                         if(screen.is_on(x_pos, y_pos)) {
                             screen.erase(x_pos, y_pos);
-                            set_flag(1);
+                            set_flag(true);
                         } else {
                             screen.fill(x_pos, y_pos);
                         }
@@ -265,77 +278,78 @@ int Emulator::single_step() {
                 ++y_pos;
                 // don't wrap around the edge of the screen
                 if(y_pos >= screen.height) break;
-                x_pos = v[x] & (screen.width - 1); // reset x position
+                x_pos = original_x; // reset x position
             }
             screen.request_update(); // refresh graphics
             break;
         }
-        case 14:
+        case 0xE:
             switch(nn) {
                 case 0x9E:
                     // 0xEX9E: skip next instruction if the key specified by
-                    // V[X] is being pressed
+                    // VX is being pressed
                     if(keys.is_pressed(v[x]))
                         pc += 2;
                     break;
                 case 0xA1:
                     // 0xEXA1: skip next instruction if the key specified by
-                    // V[X] is not being pressed
+                    // VX is not being pressed
                     if(!keys.is_pressed(v[x]))
                         pc += 2;
                     break;
             }
             break;
-        case 15:
+        case 0xF:
             switch(nn) {
                 case 0x07:
-                    // 0xFX07: set V[X] to the value of the delay timer
-                    v[x] = delay_timer;
+                    // 0xFX07: set VX to the value of the delay timer
+                    v[x] = delay;
                     break;
                 case 0x0A:
-                    // 0xFX0A: when a key is pressed, put its value in V[X]
+                    // 0xFX0A: when a key is pressed, put its value in VX
                     if(keys.any_pressed()) v[x] = keys.get_key();
                     else pc -= 2;
                     break;
                 case 0x15:
-                    // 0xFX15: set the delay timer to the value of V[X]
-                    delay_timer = v[x];
+                    // 0xFX15: set the delay timer to the value of VX
+                    delay = v[x];
                     break;
                 case 0x18:
-                    // 0xFX18: set the sound timer to the value of V[X]
-                    sound_timer = v[x];
+                    // 0xFX18: set the sound timer to the value of VX
+                    sound = v[x];
                     break;
                 case 0x1E:
-                    // 0xFX1E: add V[X] to the index register
-                    index_reg += v[x];
-                    set_flag(index_reg >= 0x1000);
+                    // 0xFX1E: add VX to the index register
+                    index += v[x];
+                    set_flag(index >= 0x1000);
+                    break;
                 case 0x29:
                     // 0xFX29: set the index register to the address of the
-                    // hexadecimal character specified by V[X] in the builtin
+                    // hexadecimal character specified by VX in the builtin
                     // font. Following the behavior of the original chip-8, we
-                    // consider just the last nibble of V[X]
-                    index_reg += font_addr + GET_NIB(v[x], 1);
+                    // consider just the last nibble of VX
+                    index += font_addr + GET_NIB(v[x], 1);
                     break;
                 case 0x33: {
-                    // 0xFX33: convert the value of V[X] into BCD and store the
+                    // 0xFX33: convert the value of VX into BCD and store the
                     // result into the address specified by the index register
                     uint8_t bcd = v[x];
-                    memory[index_reg + 2] = bcd % 10; // one's place
+                    ram[index + 2] = bcd % 10; // one's place
                     bcd /= 10;
-                    memory[index_reg + 1] = bcd % 10; // ten's place
+                    ram[index + 1] = bcd % 10; // ten's place
                     bcd /= 10;
-                    memory[index_reg] = bcd; // hundred's place
+                    ram[index] = bcd; // hundred's place
                     break;
                 }
                 case 0x55:
-                    // 0xFX55: store the values of the registers from V[0] to
-                    // V[X], inclusive, into memory starting from the address
+                    // 0xFX55: store the values of the registers from V0 to
+                    // VX, inclusive, into memory starting from the address
                     // specified by the index register. NOTE: this instruction
                     // behaves differently in different versions of chip-8.
                     // Here we adopt the chip-48 behavior, which does not
                     // change the value of the index register in its operation
                     for(uint8_t i = 0; i <= x; ++i)
-                        memory[index_reg + i] = v[i];
+                        ram[index + i] = v[i];
                     break;
                 case 0x65:
                     // 0xFX65: it does the opposite of 0xFX55, and it's subject
@@ -343,7 +357,7 @@ int Emulator::single_step() {
                     // versions of chip-8. Here we again adopt the chip-48
                     // behavior, for consistency
                     for(uint8_t i = 0; i <= x; ++i)
-                        v[i] = memory[index_reg + i];
+                        v[i] = ram[index + i];
                     break;
             }
             break;
@@ -355,5 +369,7 @@ int Emulator::single_step() {
 }
 
 #undef GET_BIT
+
 #undef GET_NIB
+
 #undef JOIN_BYTES
